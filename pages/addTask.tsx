3e39932ab3815665f177ea50ad9843d3a3e7cedf
Austin Barrows <austinm.barrows@gmail.com>
2023-02-15 21:56:14 -0500

    Implement first iteration of addTask page

--- pages/addTask.tsx
+++ pages/addTask.tsx
@@ -1,9 +1,70 @@
-import Head from "next/head";
-import Image from "next/image";
+import {
+  Card,
+  Image,
+  Text,
+  Badge,
+  Button,
+  Group,
+  Box,
+  Grid,
+  Stack,
+  TextInput,
+  Textarea,
+} from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function AddTask() {
-  return <></>;
+  return (
+    <Box className="h-screen">
+      <Box className="w-full h-full flex justify-center content-center flex-wrap">
+        <Card
+          shadow="sm"
+          p="lg"
+          radius="md"
+          withBorder
+          className="w-96 min-h-96"
+        >
+          <Grid columns={7} gutter={0}>
+            <Grid.Col span={7} className="text-3xl">
+              Add Task
+            </Grid.Col>
+            <Stack className="mt-2 w-full">
+              <TextInput
+                label="Task Name"
+                placeholder='e.g. "Do homework"'
+                required
+              />
+              <Textarea
+                label="Task Description"
+                placeholder='e.g. "Complete HCI homework assignment and then start networking HW"'
+                minRows={4}
+              />
+              <DatePicker
+                allowFreeInput
+                placeholder="Pick date"
+                label="Event date"
+                withAsterisk
+              />
+            </Stack>
+            <Grid.Col span={7}>
+              <Group mt="md" position="apart">
+                <Button color="red">Cancel</Button>
+                <Button
+                  type="submit"
+                  variant="gradient"
+                  gradient={{ from: "#045DE9", to: "#09C6F9", deg: 35 }}
+                  className="color"
+                >
+                  Save
+                </Button>
+              </Group>
+            </Grid.Col>
+          </Grid>
+        </Card>
+      </Box>
+    </Box>
+  );
 }
