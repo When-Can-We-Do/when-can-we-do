94e92f73164ab66c49c864427f9c3ab76d141025
FourNick <103899956+FourNick@users.noreply.github.com>
2023-03-27 23:38:05 -0400

    Added calendar library to project and created calendar on calendar page

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,15 +1,26 @@
 import styles from "@/styles/Home.module.css";
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 export default function Calendar() {
   return (
     <>
       <main className={styles.main}>
-        <div className=" col-span-12 content-center items-stretch">
+        <div className="h-5/6 w-full">
+          <FullCalendar
+          plugins={[ dayGridPlugin ]}
+          initialView="dayGridMonth"
+          height={"750px"}
+          // dayMinWidth={"100px"}
+          />
+        </div>
+        {/* <div className=" col-span-12 content-center items-stretch">
           <img
             src="Asset-2.png"
             className="h-[775px] block align-middle m-auto"
           ></img>
-        </div>
+        </div> */}
       </main>
     </>
   );
