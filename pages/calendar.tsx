8c765ef207f4715f2f1e410ef010847c50ef1521
FourNick <103899956+FourNick@users.noreply.github.com>
2023-03-27 23:53:26 -0400

    Added mock events to calendar

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -12,9 +12,14 @@ export default function Calendar() {
           plugins={[ dayGridPlugin ]}
           initialView="dayGridMonth"
           height={"750px"}
-          // dayMinWidth={"100px"}
+          events={[
+            { title: 'This is a task you have to do', date: '2023-03-01' },
+            { title: 'This task as well', date: '2023-03-02' }
+          ]}
           />
         </div>
+
+        // below is the image example code for the initial design idea
         {/* <div className=" col-span-12 content-center items-stretch">
           <img
             src="Asset-2.png"
